a838f02c847d832e1e63ee26c3792b4ad0f28600
Cooper <frankidyfrank3@Gmail.com>
2023-10-07 11:59:51 -0500

    made some inital changes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Meme-Team
+this project exists to help me organise my memes and search them via custom tags
+
+# Goals
+- [ ] Upload memes
+  - [ ] Add tags at time of upload
+  - [ ] save external links (strech goal)
+- [ ] View memes
+  - [ ] view external links (strech goal)
+- [ ] Edit meme tags
--- internal/accessors/models/files.go
+++ internal/accessors/models/files.go
@@ -1,10 +1,12 @@
 package models
 
-type File struct {
-	Id    string   `json:"id"`
+type Meme struct {
+	ID    string   `json:"id"`
 	Name  string   `json:"name"`
 	Notes string   `json:"notes"`
 	Links []string `json:"links"`
 	Tags  []string `json:"tags"`
-	Data  []byte   `json:"data"`
+
+	// Note If this is empty it is not a local file and the first link should be used as the meme
+	Path  string   `json:"path"`
 }
